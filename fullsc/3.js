47428ac8af22fa822aa818fb808b725a22006741
ender1324 <ender1324@abv.bg>
2020-02-20 10:35:06 +0200

    website: add all scripts for full screen

---
+++ fullsc/3.js
@@ -0,0 +1,21 @@
+ 
+// Взема „модула“
+var modal = document.getElementById('myModal');
+
+// Взема снимката и я праща на „модула“
+var img = document.getElementById('3');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Получава спана който затваря елемента
+var span = document.getElementsByClassName("close")[0];
+
+// кагато човека кликне на „спана“ то сe затваря
+span.onclick = function() { 
+  modal.style.display = "none";
+}
